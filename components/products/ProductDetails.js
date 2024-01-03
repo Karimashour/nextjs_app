4366b43817f84f303288eac64f2f4356fb4d4431
Karimashour <https://github.com/Karimashour>
2024-01-03 22:17:34 +0200

    first commit

---
+++ components/products/ProductDetails.js
@@ -0,0 +1,21 @@
+import Card from "../ui/Card";
+import classes from "./ProductDetail.module.css";
+import Image from "next/image";
+function ProductDetails(props) {
+  return (
+    <section className={classes.detail}>
+      <Card>
+        <div className={classes.image}>
+          <Image src={props.image} alt={props.name} fill={true} priority style={{
+            objectFit: 'contain', // cover, contain, none
+          }} />
+          {/* <img src={props.image} alt={props.name} /> */}
+        </div>
+        <h1>{props.name}</h1>
+        <span>${props.price}</span>
+        <p>{props.desc}</p>
+      </Card>
+    </section>
+  );
+}
+export default ProductDetails;
